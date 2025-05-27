120feaa538391a3513408933208d027e2eee6518
earlaud <etienne.arlaud@inria.fr>
2025-05-27 11:51:49 +0200

    Template interpolator

--- tests/interpolator.cpp
+++ tests/interpolator.cpp
@@ -1,7 +1,7 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include "simple-mpc/interpolator.hpp"
+#include "simple-mpc/interpolator.hxx"
 #include "test_utils.cpp"
 
 BOOST_AUTO_TEST_SUITE(interpolator)
