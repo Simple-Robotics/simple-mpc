8354e63e55224d1110292dafe9c439164dd51c4f
earlaud <etienne.arlaud@inria.fr>
2025-07-30 16:02:06 +0200

    Bind KinodynamicsIDSettings member var

--- bindings/expose-inverse-dynamics.cpp
+++ bindings/expose-inverse-dynamics.cpp
@@ -21,7 +21,17 @@ namespace simple_mpc
 
     void exposeInverseDynamics()
     {
-      bp::class_<KinodynamicsID::Settings>("KinodynamicsIDSettings", bp::init<>(bp::args("self")));
+      bp::class_<KinodynamicsID::Settings>("KinodynamicsIDSettings", bp::init<>(bp::args("self")))
+        .def_readwrite("friction_coefficient", &KinodynamicsID::Settings::friction_coefficient)
+        .def_readwrite("contact_weight_ratio_max", &KinodynamicsID::Settings::contact_weight_ratio_max)
+        .def_readwrite("contact_weight_ratio_min", &KinodynamicsID::Settings::contact_weight_ratio_min)
+        .def_readwrite("kp_base", &KinodynamicsID::Settings::kp_base)
+        .def_readwrite("kp_posture", &KinodynamicsID::Settings::kp_posture)
+        .def_readwrite("kp_contact", &KinodynamicsID::Settings::kp_contact)
+        .def_readwrite("w_base", &KinodynamicsID::Settings::w_base)
+        .def_readwrite("w_posture", &KinodynamicsID::Settings::w_posture)
+        .def_readwrite("w_contact_motion", &KinodynamicsID::Settings::w_contact_motion)
+        .def_readwrite("w_contact_force", &KinodynamicsID::Settings::w_contact_force);
 
       bp::class_<KinodynamicsID>(
         "KinodynamicsID", bp::init<const simple_mpc::RobotModelHandler &, double, const KinodynamicsID::Settings>(
