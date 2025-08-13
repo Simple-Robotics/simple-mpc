cbe8c03de769c32f4eb0cb4998b1866002356f26
earlaud <etienne.arlaud@inria.fr>
2025-08-13 11:53:22 +0200

    Reduce posture task test length

--- tests/inverse_dynamics.cpp
+++ tests/inverse_dynamics.cpp
@@ -115,7 +115,7 @@ BOOST_AUTO_TEST_CASE(KinodynamicsID_postureTask)
 
   // Change initial state
   test.q = solo_q_start(model_handler);
-  for (int i = 0; i < 10000; i++)
+  for (int i = 0; i < 1000; i++)
   {
     // Solve
     test.step();
